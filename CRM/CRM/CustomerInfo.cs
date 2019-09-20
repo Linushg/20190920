7def3ee025f1279dab9b7d020128f75258a68d32
Linus Gyllenberg <linus@sbgdata.se>
2019-09-20 13:49:00 +0200

    Base written, need to expand

---
+++ CRM/CRM/CustomerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM
+{
+    class CustomerInfo
+    {
+        public String Name { get; set; }
+        public string phonenumber { get; set; }
+        public string mail { get; set; }
+        public string favorit { get; set; }
+    }
+}
--- CRM/CRM/Program.cs
+++ CRM/CRM/Program.cs
@@ -10,6 +10,14 @@ namespace CRM
     {
         static void Main(string[] args)
         {
+            System.Collections.ArrayList CustomerInfo = new System.Collections.ArrayList();
+
+            CustomerInfo.Add(new CustomerInfo() { Name = "A", phonenumber = "B", mail = "C", favorit = "D" });
+
+            foreach (CustomerInfo tot in CustomerInfo) 
+            {
+               Console.WriteLine("{0} {1} {2} {3}", tot.Name, tot.phonenumber, tot.mail, tot.favorit);
+            }
         }
     }
 }
