d7d5751a1852aca13433ea76b6947b4e4838c0bb
Linus Gyllenberg <linus@sbgdata.se>
2019-09-21 15:37:20 +0200

    Info of workers in place

--- CRM/CRM/Program.cs
+++ CRM/CRM/Program.cs
@@ -10,9 +10,18 @@ namespace CRM
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ange namn");
+            Console.ReadLine();
+
+
             System.Collections.ArrayList CustomerInfo = new System.Collections.ArrayList();
 
-            CustomerInfo.Add(new CustomerInfo() { Name = "A", phonenumber = "B", mail = "C", favorit = "D" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Tony Stark", phonenumber = "0123456789", mail = "tony.stark@iron.man", favorit = "-" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Steve Rodgers", phonenumber = "1800728693", mail = "captain@america.com", favorit = "Yes" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Peter Parker", phonenumber = "9858484860", mail = "spider@man.de", favorit = "-" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Bruce Wayne", phonenumber = "8618561627", mail = "batman@batmail.got", favorit = "Yes" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Thor", phonenumber = "1800666999", mail = "thor@asgard.se", favorit = "Yes" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Nick Fury", phonenumber = "1964879152", mail = "fury@shield.hd", favorit = "-" });
 
             foreach (CustomerInfo tot in CustomerInfo) 
             {
