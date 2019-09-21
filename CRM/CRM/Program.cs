6e6900d1050c4a62d58f4f1f19c514499f2d775f
Linus Gyllenberg <linus@sbgdata.se>
2019-09-21 18:09:05 +0200

    Favorite now possible

--- CRM/CRM/Program.cs
+++ CRM/CRM/Program.cs
@@ -12,12 +12,12 @@ namespace CRM
         {
             System.Collections.ArrayList CustomerInfo = new System.Collections.ArrayList();
 
-            CustomerInfo.Add(new CustomerInfo() { Name = "Tony Stark", phonenumber = "0123456789", mail = "tony.stark@iron.man", favorit = "-" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Steve Rodgers", phonenumber = "1800728693", mail = "captain@america.com", favorit = "Yes" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Peter Parker", phonenumber = "9858484860", mail = "spider@man.de", favorit = "-" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Bruce Wayne", phonenumber = "8618561627", mail = "batman@batmail.got", favorit = "Yes" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Thor", phonenumber = "1800666999", mail = "thor@asgard.se", favorit = "Yes" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Nick Fury", phonenumber = "1964879152", mail = "fury@shield.hd", favorit = "-" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Tony Stark", phonenumber = "0123456789", mail = "tony.stark@iron.man", favorit = "" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Steve Rodgers", phonenumber = "1800728693", mail = "captain@america.com", favorit = "" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Peter Parker", phonenumber = "9858484860", mail = "spider@man.de", favorit = "" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Bruce Wayne", phonenumber = "8618561627", mail = "batman@batmail.got", favorit = "" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Thor", phonenumber = "1800666999", mail = "thor@asgard.se", favorit = "" });
+            CustomerInfo.Add(new CustomerInfo() { Name = "Nick Fury", phonenumber = "1964879152", mail = "fury@shield.hd", favorit = "" });
 
             bool cont = true;
             String instr;
@@ -32,17 +32,31 @@ namespace CRM
                     if (instr.Equals(tot.Name))
                     {
                         Console.WriteLine("{0} {1} {2} {3}", tot.Name, tot.phonenumber, tot.mail, tot.favorit);
+
+
+                        Console.WriteLine("Tack, favorit? (Ja/Nej)");
+                        again = Console.ReadLine();
+                        if (again.Equals("Nej"))
+                        {
+                            tot.favorit = "Nej";
+                        }
+
+                        if (again.Equals("Ja"))
+                        {
+                            tot.favorit = "Ja";
+                        }
+                        break;
                     }
                 }
+
                 Console.WriteLine("Tack, registrerad, mer? (Ja/Nej)");
                 again = Console.ReadLine();
                 if (again.Equals ("Nej"))
                 {
                     cont = false;
                 }
-                    
-
             }
+            Console.WriteLine();
         }
     }
 }
