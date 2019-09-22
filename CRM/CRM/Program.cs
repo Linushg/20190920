6ef1162f743ffd463dd1c51bc75f93ed87d3b7ae
Linus Gyllenberg <linus@sbgdata.se>
2019-09-22 17:44:46 +0200

    Done

--- CRM/CRM/Program.cs
+++ CRM/CRM/Program.cs
@@ -12,49 +12,32 @@ namespace CRM
         {
             System.Collections.ArrayList CustomerInfo = new System.Collections.ArrayList();
 
-            CustomerInfo.Add(new CustomerInfo() { Name = "Tony Stark", phonenumber = "0123456789", mail = "tony.stark@iron.man", favorit = "" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Steve Rodgers", phonenumber = "1800728693", mail = "captain@america.com", favorit = "" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Peter Parker", phonenumber = "9858484860", mail = "spider@man.de", favorit = "" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Bruce Wayne", phonenumber = "8618561627", mail = "batman@batmail.got", favorit = "" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Thor", phonenumber = "1800666999", mail = "thor@asgard.se", favorit = "" });
-            CustomerInfo.Add(new CustomerInfo() { Name = "Nick Fury", phonenumber = "1964879152", mail = "fury@shield.hd", favorit = "" });
-
             bool cont = true;
-            String instr;
             String again;
-            while (cont == true) {
+            String nstr;
+            String pstr;
+            String mstr;
+            String fstr;
+
+            while (cont == true)
+            {
                 Console.WriteLine("Ange namn");
-                instr = Console.ReadLine();
+                nstr = Console.ReadLine();
+                Console.WriteLine("Ange telefonnummer");
+                pstr = Console.ReadLine();
+                Console.WriteLine("Ange mail");
+                mstr = Console.ReadLine();
+                Console.WriteLine("Ange favorit");
+                fstr = Console.ReadLine();
 
-
-                foreach (CustomerInfo tot in CustomerInfo)
-                {
-                    if (instr.Equals(tot.Name))
-                    {
-                        Console.WriteLine("{0} {1} {2} {3}", tot.Name, tot.phonenumber, tot.mail, tot.favorit);
-
-
-                        Console.WriteLine("Tack, favorit? (Ja/Nej)");
-                        again = Console.ReadLine();
-                        if (again.Equals("Nej"))
-                        {
-                            tot.favorit = "Nej";
-                        }
-
-                        if (again.Equals("Ja"))
-                        {
-                            tot.favorit = "Ja";
-                        }
-                        break;
-                    }
-                }
-
+                CustomerInfo.Add(new CustomerInfo() { Name = nstr, phonenumber = pstr, mail = mstr, favorit = fstr });
                 Console.WriteLine("Tack, registrerad, mer? (Ja/Nej)");
                 again = Console.ReadLine();
-                if (again.Equals ("Nej"))
+                if (again.Equals("Nej"))
                 {
                     cont = false;
                 }
+
             }
 
             int favCount = 0;
